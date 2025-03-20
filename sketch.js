f9484dda91f0d5d8362b262d06d420f3be4e337b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-03-20 11:13:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+let p;
 function setup() {
   createCanvas(400, 400);
+  p = new Player(175,175,50,50,"ship.png",65);
 }
 
 function draw() {
-  background(220);
+  background(100);
+  p.display()
+  p.update()
 }
