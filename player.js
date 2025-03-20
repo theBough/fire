22aec5d083c6081ef24919da838ac088c03567ff
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-03-20 11:08:33 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,15 +1,23 @@
-function Player(x,y,w,h,fileName){
+function Player(x,y,w,h,fileName,left_direction, right_direction){
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.img = loadImage(fileName);
+  this.left_direction = left_direction;
+  this.right_direction = right_direction;
   
   this.display = function(){
     //resizing the picture based on the 
     //function that calls this object.
     this.img.resize(this.w, this.h)
     image(this.img,this.x, this.y)
+  }
+  this.update = function(){
+    //checking to see if we have to move the player left
+    if(keyIsDown(this.left_direction)){
+      this.x -= 5
+    }
   }
   
 }
