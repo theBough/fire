c349b07796a0c1b8fadea7060062344299c97e3f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-04-08 10:13:37 -0500

    Update collision.js

--- collision.js
+++ collision.js
@@ -1,24 +1,24 @@
-function wallCollision() {
+function collision(projectile,entity) {
   for (var i = 0; i < w.length; i++) {
 
-    //check if we hit the left of any wall
-    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x <= w[i].x + w[i].w && p.x >= w[i].x) {
-      p.x += 5
+    //check if we hit the left
+    if (entity.y <= projectile[i].y + projectile[i].h && entity.y + entity.h >= projectile[i].y && entity.x <= w[i].x + projectilei].w && entity.x >= projectile[i].x) {
+      entity.isAlive = false
     }
 
-    //check if we hit the right of any wall
-    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x + p.w >= w[i].x && p.x <= w[i].x + w[i].w) {
-      p.x -= 5
+    //check if we hit the right
+    if (entity.y <= projectile[i].y + projectile[i].h && entity.y + entity.h >= projectile[i].y && entity.x + entity.w >= projectile[i].x && entity.x <= projectile[i].x + projectile[i].w) {
+      entity.isAlive = false
     }
 
 
-    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y <= w[i].y + w[i].h && p.y >= w[i].y) {
-      p.y += 5;
+    if (entity.x <= projectile[i].x + projectile[i].w && entity.x + entity.w >= projectile[i].x && entity.y <= projectile[i].y + projectile[i].h && entity.y >= projectile[i].y) {
+      entity.isAlive = false
     }
 
-    //check if we hit the top of any wall
-    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y + p.h >= w[i].y && p.y <= w[i].y + w[i].h) {
-      p.y -= 5;
+    //check if we hit the top
+    if (entity.x <= projectile[i].x + projectile[i].w && entity.x + entity.w >= projectile[i].x && entity.y + entity.h >= w[i].y && entity.y <= projectile[i].y + projectile[i].h) {
+      entity.isAlive = false
     }
 
   }//end loop
