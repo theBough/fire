20957324d7cd3030640b03c02a2552e1ba0c7509
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-04-08 10:23:01 -0500

    Update collision.js

--- collision.js
+++ collision.js
@@ -1,22 +1,22 @@
 function collision(projectile,entity) {
   for (var i = 0; i < projectile.length; i++) {
 
-    //check if we hit the left
+    //check if we hit left side
     if (entity.y <= projectile[i].y + projectile[i].h && entity.y + entity.h >= projectile[i].y && entity.x <= projectile[i].x + projectile[i].w && entity.x >= projectile[i].x) {
       entity.isAlive = false
     }
 
-    //check if we hit the right
+    //check if we hit right side
     if (entity.y <= projectile[i].y + projectile[i].h && entity.y + entity.h >= projectile[i].y && entity.x + entity.w >= projectile[i].x && entity.x <= projectile[i].x + projectile[i].w) {
       entity.isAlive = false
     }
 
-
+    //check if we hit bottom side
     if (entity.x <= projectile[i].x + projectile[i].w && entity.x + entity.w >= projectile[i].x && entity.y <= projectile[i].y + projectile[i].h && entity.y >= projectile[i].y) {
       entity.isAlive = false
     }
 
-    //check if we hit the top
+    //check if we hit top side
     if (entity.x <= projectile[i].x + projectile[i].w && entity.x + entity.w >= projectile[i].x && entity.y + entity.h >= projectile[i].y && entity.y <= projectile[i].y + projectile[i].h) {
       entity.isAlive = false
     }
